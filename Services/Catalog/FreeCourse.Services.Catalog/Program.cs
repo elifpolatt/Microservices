d7795b5b19e58ed375a9dcfb70a8c94ac6fa1eed
Elif Polat <elif7polat@outlook.com>
2024-07-15 17:52:16 +0300

    json dosyası

--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,4 +1,7 @@
 using System.Reflection;
+using FreeCourse.Services.Catalog.Settings;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +11,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//AutoMapper 
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+//appsetting.json dosyasý ýcýndeký verý tabaný býlgýlerýný býr sýnýf ýcerýsýnden alacagýz.
+//Classlarýn ýcýne gýdýp IOptions<DatabaseSettings> dýyerek bunlarý set edeerek kullanabýlýyoruz fakat dolu olarak json dosyasýndan gelmelý
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings")); 
+builder.Services.AddSingleton<IDatabaseSettings>(sp => //service provider
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
+
 
 var app = builder.Build();
 
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Settings/IDatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace FreeCourse.Services.Catalog.Settings
+{
+    public interface IDatabaseSettings
+    {
+        //appsettings.json dosyası ıcındekı verı tabanı bılgılerını almak ıcın bu ınterface kullanılacak.
+
+        public string CourseCollectionName { get; set; }
+        public string CategoryCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+       
+    }
+}
